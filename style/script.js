0bec30c14a5b617d6aa713becf5f62f1b141cd20
VSviviane <fisiovivianes@gmail.com>
2024-08-20 14:42:21 -0300

    Desafio challenge

---
+++ style/script.js
@@ -0,0 +1,48 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = ""; // Limpar o campo de entrada após a encriptação
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = ""; // Limpar o campo de entrada após a desencriptação
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada;
+}
+
+function copiarTexto() {
+    const textoParaCopiar = document.getElementById('meuTexto').innerText;
+    navigator.clipboard.writeText(textoParaCopiar).then(() => {
+      alert('Texto copiado!');
+    }).catch(err => {
+      console.error('Erro ao copiar o texto: ', err);
+    });
+  }
+  
